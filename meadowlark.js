e2a062569817884b9a3c90d51ffc21670c585445
witch-factory <soakdma37@gmail.com>
2022-03-06 16:08:30 +0900

    핸들바 사용해보기, static 미들웨어 사용

--- meadowlark.js
+++ meadowlark.js
@@ -1,31 +1,34 @@
 import express from 'express'
+import {engine} from 'express-handlebars';
+import path from 'path'
+const __dirname=path.resolve()
 
-const app=express();
+const app=express()
+
+app.engine('handlebars', engine())
+app.set('view engine', 'handlebars')
+app.use(express.static(__dirname + '/public'))
+//public 디렉터리에 있는 건 조건없이 클라이언트로 전송된다
 const port=process.env.PORT || 3000
 
 app.get('/', (req, res)=>{
-  res.type('text/plain')
-  res.send('Meadowlark Travel')
+  res.render('home')
 })
 
 app.get('/about', (req, res)=>{
-  res.type('text/plain')
-  res.send('About Meadowlark Travel')
+  res.render('about')
 })
 
 //custom 404 page
 app.use((req, res)=>{
-  res.type('text/plain')
   res.status(404)
-  res.send('404-Not Found')
+  res.render('404')
 })
 
 //custom 500 page
 app.use((err, req, res, next)=>{
-  console.error(err.statusMessage)
-  res.type('text/plain')
   res.status(500)
-  res.send('500-Server Error')
+  res.render('500')
 })
 
 app.listen(port, ()=>console.log(
