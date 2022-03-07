d4cd39bbfe5571cdd2ffa3551c57bd4de02fd2e9
witch-factory <soakdma37@gmail.com>
2022-03-07 17:09:52 +0900

    jest 테스트를 위한 리팩토링

--- meadowlark.js
+++ meadowlark.js
@@ -1,14 +1,8 @@
 import express from 'express'
-import {engine} from 'express-handlebars';
+import {engine} from 'express-handlebars'
 import path from 'path'
 const __dirname=path.resolve()
-
-const fortunes=[
-  "Conquer your fears or they will conquer you.",
-  "River needs springs.",
-  "Do not fear what you don't know.",
-  "You'll have a pleasant surprise."
-]
+import handlers from "./lib/handlers.js"
 
 const app=express()
 
@@ -18,26 +12,15 @@ app.use(express.static(__dirname + '/public'))
 //public 디렉터리에 있는 건 조건없이 클라이언트로 전송된다
 const port=process.env.PORT || 3000
 
-app.get('/', (req, res)=>{
-  res.render('home')
-})
+app.get('/', handlers.home)
 
-app.get('/about', (req, res)=>{
-  const randomFortune=fortunes[Math.floor(Math.random()*fortunes.length)]
-  res.render('about', {fortune : randomFortune})
-})
+app.get('/about', handlers.about)
 
 //custom 404 page
-app.use((req, res)=>{
-  res.status(404)
-  res.render('404')
-})
+app.use(handlers.notFound)
 
 //custom 500 page
-app.use((err, req, res, next)=>{
-  res.status(500)
-  res.render('500')
-})
+app.use(handlers.serverError)
 
 app.listen(port, ()=>console.log(
   `Express started on http://localhost:${port}`
