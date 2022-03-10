f081b48ae73c37a5278bb035b5d0d56ed3484676
witch-factory <soakdma37@gmail.com>
2022-03-11 02:00:07 +0900

    6장 공부중

--- meadowlark.js
+++ meadowlark.js
@@ -16,6 +16,13 @@ app.get('/', handlers.home)
 
 app.get('/about', handlers.about)
 
+app.get('/headers', (req, res)=>{
+  res.type('text/plain')
+  const headers=Object.entries(req.headers)
+    .map(([key, value])=>`${key}: ${value}`)
+  res.send(headers.join('\n'))
+})
+
 //custom 404 page
 app.use(handlers.notFound)
 
